d290522497e6c6a34c9b8f7dab3a04a525edecb2
Edson Ribeiro <101573671+EdsonLRS@users.noreply.github.com>
2022-04-12 10:03:04 -0300

    Add files via upload

---
+++ pecas.js
@@ -0,0 +1,21 @@
+let pesoPeca = 200
+let numeroPecas = 8 
+let nomePecas = 'Disco de Freio'
+
+if(pesoPeca > 100){
+ // Podemos cadastrar 
+ console.log ("Peso adequado, podemor cadastrar")
+
+} else {
+    // Não podemos cadastrar
+    console.log ("Peso menor que 100g, não podemor cadastar")
+}
+
+if (numeroPecas >= 10) {
+    // Capacidade máxima atingida
+    console.log ("Capacidade máxima atingida")
+} else {
+    // Ainda há espaço para cadastro
+    console.log ("Ainda há espaço para cadastro")
+
+} 
